08c3f63f63764c07708e706c1435917b0d6fc841
manojkumar-Vk2k5 <manojkumar.v10322@gmail.com>
2025-09-08 00:15:16 +0530

    Update App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ShoppingApp from './home';
+import ShoppingApp from './pages/home';
 import ScanPage from './pages/ScanPage';
 import MapPage from './pages/MapPage';
 import ServicesPage from './pages/ServicesPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import BottomNav from './Navigation/BottomNav';
 
 // function App() {
@@ -18,18 +20,28 @@ import BottomNav from './Navigation/BottomNav';
 //     </>
 //   )
 // }
+
+function AppRoutesWithNav() {
+  // Nav bar should be visible on all pages
+  return (
+    <div className="relative min-h-screen pb-16">
+      <Routes>
+        <Route path="/" element={<ShoppingApp />} />
+        <Route path="/scan" element={<ScanPage />} />
+        <Route path="/map" element={<MapPage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+  <BottomNav />
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="relative min-h-screen pb-16">
-        <Routes>
-          <Route path="/" element={<ShoppingApp />} />
-          <Route path="/scan" element={<ScanPage />} />
-          <Route path="/map" element={<MapPage />} />
-          <Route path="/services" element={<ServicesPage />} />
-        </Routes>
-        <BottomNav />
-      </div>
+      <AppRoutesWithNav />
     </BrowserRouter>
   );
 }
