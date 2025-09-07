4c7d500181a715a32ed1920ebc78ce1b21313085
manojkumar-Vk2k5 <manojkumar.v10322@gmail.com>
2025-09-08 00:08:06 +0530

    Create ScanPage.jsx

---
+++ app/src/pages/ScanPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ScanPage() {
+  return (
+    <div className="w-full max-w-sm mx-auto min-h-screen flex items-center justify-center">
+      <h2 className="text-2xl font-bold text-gray-700">Scan Page</h2>
+    </div>
+  );
+}
